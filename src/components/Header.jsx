64846eb047fadd387848e6deab1beca9fa4831b5
Egorych <mikhalevichegor@gmail.com>
2021-07-01 15:54:48 +0300

    create Home.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,39 +1,50 @@
 import React, {Component} from "react";
 import {Button, Container, Form, FormControl, Nav, Navbar} from "react-bootstrap";
 import logo from "./logo/logo192.png"
+import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
 
 export function Header() {
     // render() {
     return (
+        <>
         <Navbar collapseOnSelect expand={"md"} bg={"dark"} variant={"dark"}>
             <Container>
                 <Navbar.Brand href={"/"}>
                     <img
                         src={logo}
-                        alt="Logo"
-                        width={"30"}
                         height={"30"}
+                        width={"30"}
                         className={"d-inline-block align-top"}
+                        alt="Logo"
                     />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls={"responsive-navbar-nav"}/>
                 <Navbar.Collapse id={"responsive-navbar-nav"}>
                     <Nav className={"mr-auto"}>
-                        <Nav.Link href={"/"}>Home</Nav.Link>
-                        <Nav.Link href={"/"}>About us</Nav.Link>
-                        <Nav.Link href={"/"}>Contacts</Nav.Link>
-                        <Nav.Link href={"/"}>Blog</Nav.Link>
+                        <Nav.Link href={"/home"}>Home</Nav.Link>
+                        <Nav.Link href={"/about"}>About us</Nav.Link>
+                        <Nav.Link href={"/contacts"}>Contacts</Nav.Link>
+                        <Nav.Link href={"/blog"}>Blog</Nav.Link>
                     </Nav>
-                    <Form inline>
+                    <Form inline={"inline"}>
                         <FormControl
                             type={"text"}
                             placeholder={"Search"}
-                            className={"mr-sm-2"}
+                            className={"mr-auto"}
                         />
                         <Button variant={"outline-info"}>Search</Button>
                     </Form>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+            <Router>
+                <Switch>
+                    <Route exact path={"/home"} component={Home}/>
+                    <Route exact path={"/about"} component={About}/>
+                    <Route exact path={"/contacts"} component={Contacts}/>
+                    <Route exact path={"/blog"} component={Blog}/>
+                </Switch>
+            </Router>
+        </>
     )
 }
