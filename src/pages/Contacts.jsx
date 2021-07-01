f54b15df2598c0ad14766facbdd7432c63e84f16
Egorych <mikhalevichegor@gmail.com>
2021-07-01 19:31:15 +0300

    feedback form

--- src/pages/Contacts.jsx
+++ src/pages/Contacts.jsx
@@ -1,9 +1,26 @@
 import React from "react";
+import {Container, Form, FormCheck, FormControl, FormGroup, FormLabel, FormText} from "react-bootstrap";
+import {Button} from "react-bootstrap";
 
 export function Contacts() {
     return (
-        <div>
-
-        </div>
+        <Container style={{ width: "500px"}}>
+            <h1 className={"t-center"}>Contact us</h1>
+            <Form>
+            <FormGroup controlId={"formBasicEmail"}>
+                <FormLabel>Email address</FormLabel>
+                <FormControl type={"email"} placeholder={"Enter email"}/>
+                <FormText>We'll never share your email with anyone else</FormText>
+            </FormGroup>
+            <FormGroup controlId={"formBasicPassword"}>
+                <FormLabel>Example textarea</FormLabel>
+                <FormControl as={"textarea"} rows={"3"}/>
+            </FormGroup>
+            <FormGroup controlId={"formBasicCheckbox"}>
+                <FormCheck type={"checkbox"} label={"Check me out"}/>
+            </FormGroup>
+            <Button variant={"primary"} type={"submit"}>Submit</Button>
+            </Form>
+        </Container>
     )
 }
