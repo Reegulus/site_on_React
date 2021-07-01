594f1b378026170577988a44cf3c7e462b1a43f1
Egorych <mikhalevichegor@gmail.com>
2021-07-01 20:58:11 +0300

    feedback form

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,55 @@
 import React from "react";
 import {CarouselBox} from "../components/CarouselBox";
+import {Button, Card, CardDeck, CardImg, Container} from "react-bootstrap";
 
 export function Home() {
     return (
-        <div>
+        <>
             <CarouselBox/>
-        </div>
+            <Container fluid="md">
+                <h2 className={"text-center m-4"}>Our team</h2>
+                <CardDeck>
+                    <Card>
+                        <CardImg
+                            variant={"top"}
+                            src={"https://images.wallpaperscraft.ru/image/noutbuk_chashka_ochki_rastenie_114948_1920x1080.jpg"}/>
+                        <Card.Body>
+                            <Card.Title>Developers</Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur corporis,
+                                deleniti deserunt dicta dolores excepturi exercitationem.
+                            </Card.Text>
+                            <Button variant={"primary"}>About team</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <CardImg
+                            variant={"top"}
+                            src={"https://images.wallpaperscraft.ru/image/noutbuk_chashka_ochki_rastenie_114948_1920x1080.jpg"}/>
+                        <Card.Body>
+                            <Card.Title>Developers</Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur corporis,
+                                deleniti deserunt dicta dolores excepturi exercitationem.
+                            </Card.Text>
+                            <Button variant={"primary"}>About team</Button>
+                        </Card.Body>
+                    </Card>
+                    <Card>
+                        <CardImg
+                            variant={"top"}
+                            src={"https://images.wallpaperscraft.ru/image/noutbuk_chashka_ochki_rastenie_114948_1920x1080.jpg"}/>
+                        <Card.Body>
+                            <Card.Title>Developers</Card.Title>
+                            <Card.Text>
+                                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur corporis,
+                                deleniti deserunt dicta dolores excepturi exercitationem.
+                            </Card.Text>
+                            <Button variant={"primary"}>About team</Button>
+                        </Card.Body>
+                    </Card>
+                </CardDeck>
+            </Container>
+        </>
     )
 }
